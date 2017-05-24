d733dbf5e92def17dea5ae045406d346a6e4d8dd
Bartosz Miklaszewski <bartosz@miklaszewski.com>
2017-05-24 20:44:43 +0100

    Delete test.tf

--- projects/bart/test.tf
+++
@@ -1,8 +0,0 @@
-# roles/modules/providers/test.tf
-
-provider "openstack" {
-     user_name = "bartosz@miklaszewski.com"
-     tenant_name = "test1"
-     auth_url = "https://compute.datacentred.io:5000/v2.0"
-     password = "passwordgoeshere"
-}
